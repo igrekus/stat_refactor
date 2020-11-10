74ed4249956f3b255116f6873795e5bcccd1e8f3
igrekus <upload234@mail.ru>
2020-11-10 16:34:54 +0300

    REF: simplify looping logic
    - rename function argument to clarify the purpose
    - iterate over dict items, to remove value lookup
    - rename loop variable for clarity

--- main.py
+++ main.py
@@ -4,24 +4,22 @@ import openpyxl as xl
 
 from take_data import parse_raw_data
 
-def enter_data_to_excel(data):
+
+def enter_data_to_excel(parsed_data):
     cap_name = '№	f, MHz	P, dBm	IG, mA	ID, A	Gain, dB	КПД, %	Pвых, W'.split('\t')
 
-    for i in data.keys():
+    for file_name, file_data in parsed_data.items():
+        print(file_name[:-4], file_data)
+
         wb = xl.Workbook()
-
-        print(i[:-4], data[i])
-
-        meas = data[i]
-
         ws = wb.active
-        wb.title = i[:-4]
+        wb.title = file_name[:-4]
         cap = False  # Наличие шапки(в первую строку)
         for block in range(1, 9):
             if not cap:
                 ws.cell(1, block, cap_name[block - 1])
 
-        for number, d in enumerate(meas['data']):
+        for number, d in enumerate(file_data['data']):
             ws.cell(int(number) + 2, 1, number)
 
             for x_instr in range(1, 8):
@@ -29,7 +27,7 @@ def enter_data_to_excel(data):
 
         ws.cell(1, 9, "max number")
         ws.cell(1, 10, number)
-        wb.save(i[:-4] + '.xlsx')
+        wb.save(file_name[:-4] + '.xlsx')
         wb.close()
 
 
@@ -39,7 +37,9 @@ def main(args):
         return
 
     path = args[1]
-    print(parse_raw_data(path))
+
+    res = parse_raw_data(path)
+
     enter_data_to_excel(res)
 
 
