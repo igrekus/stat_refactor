4ddadc1a7a94a1b45674d2d0e6d226aa981fb765
igrekus <upload234@mail.ru>
2020-11-10 15:21:24 +0300

    REF: remove dead code, rearrange functions

--- take_data.py
+++ take_data.py
@@ -2,11 +2,14 @@ import os
 import re
 
 
-def _parse_line(line):
-    # return normalized data raw
-    # freq in GHz; pow rounded to 3rd place; the rest of the data as is
-    data = line.replace(',', '.').split('\t')[1:]
-    return [round(float(data[0]) / 1E+09, 2), round(float(data[1]), 3)] + [float(v) for v in data[2:]]
+def parse_raw_data(path):
+    return {file: _parse_file(file) for file in _filter_sources(path)}
+
+
+def _parse_file(file):
+    with open(file, 'rt', encoding='utf-8') as f:
+        raw_header, *raw_data = _filter_raw_data(f.readlines())
+    return {'header': [str(el) for el in raw_header.split('\t')[1:]], 'data': [_parse_line(line) for line in raw_data]}
 
 
 def _filter_sources(path):
@@ -22,16 +25,8 @@ def _filter_raw_data(lines):
     return [l.strip() for l in lines if l.startswith('№') or re.compile(r'^(-?\d+,\d+\s)+$').match(l)]
 
 
-def _parse_file(file):
-    with open(file, 'rt', encoding='utf-8') as f:
-        raw_header, *raw_data = _filter_raw_data(f.readlines())
-    return {'header': [str(el) for el in raw_header.split('\t')[1:]], 'data': [_parse_line(line) for line in raw_data]}
-
-
-def parse_raw_data(path):
-    return {file: _parse_file(file) for file in _filter_sources(path)}
-
-
-# x = take_data()
-# for i in x.keys():
-#     print(i[:-4], x[i])
+def _parse_line(line):
+    # return normalized data raw
+    # freq in GHz; pow rounded to 3rd place; the rest of the data as is
+    data = line.replace(',', '.').split('\t')[1:]
+    return [round(float(data[0]) / 1E+09, 2), round(float(data[1]), 3)] + [float(v) for v in data[2:]]
