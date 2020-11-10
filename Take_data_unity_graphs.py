134b7f1e4ff264168b9d9b35dae4cd3b0fac8e01
igrekus <upload234@mail.ru>
2020-11-10 17:29:03 +0300

    MISC: clean up syntax errors

--- Take_data_unity_graphs.py
+++ Take_data_unity_graphs.py
@@ -2,23 +2,18 @@ import openpyxl as xl
 import os
 
 
-
 def take_data_excel():
     if 'excel' not in os.getcwd():
         os.chdir('excel')
-        Datas = os.listdir()
 
-    else:
-        Datas = os.listdir()
+    Datas = os.listdir()
 
     if 'unity.xlsx' in Datas:
         wb_unity = xl.load_workbook('unity.xlsx')  # Файл соединенный из других файлов
     else:
-        return False
         wb_unity = xl.Workbook()
+        return False
 
     if len(Datas) == 0:
         print('No unity.xlsx file')
         return False
-
-    if
