8e5e99012b23f4cb6e95c74387ed71469dde469e
igrekus <upload234@mail.ru>
2020-11-11 17:14:24 +0300

    FEAT: implement compiled workbook gen
    - all parsed datasets
    - consolidated stats table
    - plots for consolidated data

--- put_data.py
+++ put_data.py
@@ -1,5 +1,9 @@
+import itertools
 import os
 import openpyxl
+
+from openpyxl.chart import LineChart, Reference
+from string import ascii_uppercase
 
 
 def export_to_excel(parsed_data, export_folder='excel'):
@@ -27,5 +31,46 @@ def export_to_excel(parsed_data, export_folder='excel'):
         wb.save(f'{os.path.join(export_folder, out_file)}.xlsx')
         wb.close()
 
-        wb_compiled.save(f'{os.path.join(export_folder, "compiled")}.xlsx')
-        wb_compiled.close()
+    first = next(iter(parsed_data.values()))
+    freqs = [f for f, *rest in first['data']]
+
+    header = ['f, GHz', *[f'sheet {i + 1}' for _ in range(len(first['data'][0]) - 1) for i in range(len(parsed_data))]]
+    footer = ['', *[h for h in first['header'][1:] for _ in range(len(parsed_data))]]
+
+    data = list(zip(*[v['data'] for v in parsed_data.values()]))
+    data1 = [[vs[1:] for vs in d] for d in data]
+    data2 = [list(itertools.chain.from_iterable(list(zip(*d)))) for d in data1]
+    data3 = [[f] + v for f, v in zip(freqs, data2)]
+
+    ws_plots.append(header)
+    for row in data3:
+        ws_plots.append(row)
+    ws_plots.append(footer)
+
+    base_row = 2
+    base_col = 2
+    sets = len(parsed_data)
+    rows = len(freqs)
+    data_blocks = [
+        [[base_row, base_col + i * sets], [base_row + rows - 1, base_col + sets + i * sets - 1]]
+        for i in range(len(first['header']) - 1)
+    ]
+
+    for block in data_blocks:
+
+        (row1, col1), (row2, col2) = block
+
+        chart = LineChart()
+        chart.title = first['header'][1:][0]
+        chart.style = 2
+
+        dt = Reference(ws_plots, min_row=row1 - 1, min_col=col1, max_row=row2, max_col=col2)
+        xs = Reference(ws_plots, min_row=2, min_col=1, max_row=row2)
+
+        chart.add_data(dt, titles_from_data=True)
+        chart.set_categories(xs)
+
+        ws_plots.add_chart(chart, f'{ascii_uppercase[col1 - 1]}10')
+
+    wb_compiled.save(f'{os.path.join(export_folder, "compiled")}.xlsx')
+    wb_compiled.close()
